5c86491c78596d26704cc3fa8117a1111722567b
juquann <82115184+juquann@users.noreply.github.com>
2021-04-15 19:31:38 -0400

    New Comment
    Add description of method

--- bikeshare.py
+++ bikeshare.py
@@ -5,7 +5,7 @@ import numpy as np
 CITY_DATA = { 'chicago': 'chicago.csv',
               'new york city': 'new_york_city.csv',
               'washington': 'washington.csv' }
-
+#define method
 def get_filters():
     """
     Asks user to specify a city, month, and day to analyze.
